31366eec21ae03e86116aff550421309f66aa920
Pavel <pavel.1504@mail.ru>
2025-10-18 02:12:26 +1000

    мои решения задач3

--- problems/03-parentheses.js
+++ problems/03-parentheses.js
@@ -1,42 +1,34 @@
-    /**
-     * Строка со скобками считается валидной, если на каждую закрывающую скобку имеется ранее открытая
-     * и на каждую ранее открытую имеется закрывающая.
-     *
-     * Напишите функцию parentheses(value) проверяющую строку со скобками на валидность.
-     *
-     * Пример:
-     *
-     * parentheses('') === false
-     * parentheses('()()') === true
-     * parentheses('(()())') === true
-     * parentheses('(()') === false
-     * parentheses(')') === false
-     *
-     * @param {string} value
-     * @returns {boolean}
-     */
-    function parentheses(value) {
-        if (value.length === 0) return false;
+/**
+ * Строка со скобками считается валидной, если на каждую закрывающую скобку имеется ранее открытая
+ * и на каждую ранее открытую имеется закрывающая.
+ *
+ * Напишите функцию parentheses(value) проверяющую строку со скобками на валидность.
+ *
+ * Пример:
+ *
+ * parentheses('') === false
+ * parentheses('()()') === true
+ * parentheses('(()())') === true
+ * parentheses('(()') === false
+ * parentheses(')') === false
+ *
+ * @param {string} value
+ * @returns {boolean}
+ */
+function parentheses(value) {
+    if (value.length === 0) return false;
+    let count = 0;
 
-        let count = 0;
-
-        for (let i = 0; i<value.length;  i++){
-            const car = value[i];
-            if (car === '('){
-                count ++;
-            '0'} else if (char === `)`) {
-                count   --
-            }
-            if(count < 0 ){
-            return    false   
-            }
-        }  
-
-    if 
-
-
-
-        return count === 0;
+    for (let i = 0; i < value.length; i++) {
+        const char = value[i];
+        if (char === "(") {
+            count++;
+        } else if (char === ")") {
+            count--;
+        }
+        if (count < 0) return false;
     }
+    return count === 0;
+}
 
-    module.exports = parentheses        ;
+module.exports = parentheses;
