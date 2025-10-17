42b45eb6fd023ae952143016e44f843a4d480fd7
Pavel <pavel.1504@mail.ru>
2025-10-18 02:56:39 +1000

    мои решения задач4

--- problems/04-hex-to-rgb.js
+++ problems/04-hex-to-rgb.js
@@ -14,7 +14,18 @@
  * @returns {string}
  */
 function hexToRgb(color) {
-    return undefined;
+    let hex = color.slice(1);
+    if (hex.length === 3) {
+        hex = hex
+            .split("")
+            .map((ch) => ch + ch)
+            .join("");
+    }
+    const r = parseInt(hex.slice(0, 2), 16);
+    const g = parseInt(hex.slice(2, 4), 16);
+    const b = parseInt(hex.slice(4, 6), 16);
+
+    return `rgb(${r}, ${g}, ${b})`;
 }
 
 module.exports = hexToRgb;
